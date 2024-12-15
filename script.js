5e5ba97b35be5c821f1ddad1daf65e597c92b67b
Алексей Чаадаев <104098139+Tsygaika@users.noreply.github.com>
2024-12-15 21:52:24 +0300

    normal date update

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
-<!doctype html>
-<html lang="ru">
+// класс для создание таймера обратного отсчета
+class CountdownTimer {
+    constructor(deadline, cbChange, cbComplete) {
+      this._deadline = deadline;
+      this._cbChange = cbChange;
+      this._cbComplete = cbComplete;
+      this._timerId = null;
+      this._out = {
+        days: '', hours: '', minutes: '', seconds: '',
+        daysTitle: '', hoursTitle: '', minutesTitle: '', secondsTitle: ''
+      };
+      this._start();
+    }
+    static declensionNum(num, words) {
+      return words[(num % 100 > 4 && num % 100 < 20) ? 2 : [2, 0, 1, 1, 1, 2][(num % 10 < 5) ? num % 10 : 5]];
+    }
+    _start() {
+      this._calc();
+      this._timerId = setInterval(this._calc.bind(this), 1000);
+    }
+    _calc() {
+      const diff = this._deadline - new Date();
+      const days = diff > 0 ? Math.floor(diff / 1000 / 60 / 60 / 24) : 0;
+      const hours = diff > 0 ? Math.floor(diff / 1000 / 60 / 60) % 24 : 0;
+      const minutes = diff > 0 ? Math.floor(diff / 1000 / 60) % 60 : 0;
+      const seconds = diff > 0 ? Math.floor(diff / 1000) % 60 : 0;
+      this._out.days = days < 10 ? '0' + days : days;
+      this._out.hours = hours < 10 ? '0' + hours : hours;
+      this._out.minutes = minutes < 10 ? '0' + minutes : minutes;
+      this._out.seconds = seconds < 10 ? '0' + seconds : seconds;
+      this._out.daysTitle = CountdownTimer.declensionNum(days, ['день', 'дня', 'дней']);
+      this._out.hoursTitle = CountdownTimer.declensionNum(hours, ['час', 'часа', 'часов']);
+      this._out.minutesTitle = CountdownTimer.declensionNum(minutes, ['минута', 'минуты', 'минут']);
+      this._out.secondsTitle = CountdownTimer.declensionNum(seconds, ['секунда', 'секунды', 'секунд']);
+      this._cbChange ? this._cbChange(this._out) : null;
+      if (diff <= 0) {
+        clearInterval(this._timerId);
+        this._cbComplete ? this._cbComplete() : null;
+      }
+    }
+  }
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Таймер счастья</title>
+  document.addEventListener('DOMContentLoaded', () => {
 
-    <link rel="shortcut icon" href="chronometer.png">
-    <link rel='stylesheet' href="style.css">
-    <script src="script.js"></script>
-</head>
+    // .timer-2 (до конца месяца)
+    const elDays2 = document.querySelector('.timer-2 .timer__days');
+    const elHours2 = document.querySelector('.timer-2 .timer__hours');
+    const elMinutes2 = document.querySelector('.timer-2 .timer__minutes');
+    const elSeconds2 = document.querySelector('.timer-2 .timer__seconds');
+    const deadline2 = new Date(2024, 11, 26,16,0,0);
+    new CountdownTimer(deadline2, (timer) => {
+      elDays2.textContent = timer.days;
+      elHours2.textContent = timer.hours;
+      elMinutes2.textContent = timer.minutes;
+      elSeconds2.textContent = timer.seconds;
+      elDays2.dataset.title = timer.daysTitle;
+      elHours2.dataset.title = timer.hoursTitle;
+      elMinutes2.dataset.title = timer.minutesTitle;
+      elSeconds2.dataset.title = timer.secondsTitle;
+    }, () => {
+      document.querySelector('.timer-2 .timer__result').innerHTML = "<h1 class='final_text'>Счастливая жизнь наступила!!!</h1><video class='final_video' src='smiles.mp4' loop autoplay muted preload style='line-height: 0'></video>";
+    });
 
-<body>
-  <div class="timer timer-2">
-    <h1 class="text">
-        До счастливой жизни осталось:
-    </h1>
-    <div class="timer__items">
-      <div class="timer__item timer__days">00</div>
-      <div class="timer__item timer__hours">00</div>
-      <div class="timer__item timer__minutes">00</div>
-      <div class="timer__item timer__seconds">00</div>
-    </div>
-    <div class="timer__result"></div>
-  </div>
-
-</body>
-</html>
+  });
